5c439d233feb015a0aabf505caeed534bc9693ad
Luke aka SwissalpS <Luke@SwissalpS.ws>
2021-12-06 16:28:05 +0100

    added elapsed time

--- module_time.lua
+++ module_time.lua
@@ -1,14 +1,20 @@
 -- module time --
 -- by SwissalpS --
 -- displays in-game-time.
+local joined = core.get_us_time()
+
 local function update(index)
 
 	local fT = core.get_timeofday()
 	local iH = math.floor(24 * fT)
 	local iM = (fT * 24 - iH) * 60
 
-	return 'Time: ' .. tmi.twoDigitNumberString(iH)
-		.. ':' .. tmi.twoDigitNumberString(iM)
+	local iE = (core.get_us_time() - joined) * .000001
+	local sE = string.format('%02i:%02i:%02i', iE / 3600, iE / 60 % 60, iE % 60)
+
+	return 'Time: ' .. string.format('%02i', iH)
+		.. ':' .. string.format('%02i', iM)
+		.. '\nElapsed: ' .. sE
 
 end -- update
 
@@ -17,8 +23,8 @@ tmi.addModule({
 	id = 'time',
 	title = 'time',
 	value = 'time module',
+	onInit = init,
 	onUpdate = update,
 })
 
 --print('module time loaded')
-
