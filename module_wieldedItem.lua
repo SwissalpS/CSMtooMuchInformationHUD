06eba39c86a9c971cb520144b998ec009fed5f03
Luke aka SwissalpS <Luke@SwissalpS.ws>
2021-12-12 00:07:10 +0100

    empty hand and itemstring

--- module_wieldedItem.lua
+++ module_wieldedItem.lua
@@ -8,6 +8,8 @@ local function update(index)
 	local oWI = tmi.player:get_wielded_item()
 	local oMeta = oWI:get_meta()
 	local sItemstring = oWI:get_name()
+	if '' == sItemstring then return 'Empty Hand\n' end
+
 	local iCount = oWI:get_count()
 	local sDescription = oWI:get_description()
 	local iWear = oWI:get_wear()
@@ -22,6 +24,7 @@ local function update(index)
 	if '' ~= sPos then sWear = sPos end
 
 	return iCount .. ' ' .. (sDescription or sItemstring)
+			.. '\nItemstr: ' .. sItemstring
 			.. '\nWear: ' .. sWear
 			.. '\n'
 
